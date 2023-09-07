a1a03406787024de2ad70de73db2c1f4a2555d48
Niharika Gurnani <niharika.gurnani@solvezy.com>
2023-09-07 19:41:26 +0530

    Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
-  {path : 'orders', component : OrdersComponent}
+  {path : 'orders', component : OrdersComponent},
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
 ];
 
 @NgModule({
